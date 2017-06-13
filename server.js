45241e4f397d1ca908e128c33ce6c9b888ec4017
ibrahimayman <eng.ibrahim.cer@hotmail.com>
2017-06-13 22:22:39 +0200

    hbs

---
+++ server.js
@@ -0,0 +1,77 @@
+/**
+ * Created by Ibrahim Ayman on 13/06/2017.
+ */
+
+var express = require("express");
+var hbs = require("hbs");
+var app = express();
+var fs = require('fs');
+
+//******************************************************
+app.set('view engine', 'hbs'); // handelbarjs library
+hbs.registerPartials(__dirname + '/views/partials');
+// Hbs Helper Methods
+hbs.registerHelper('GetCurrentYear', function () {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screemIt', function (text) {
+    return text.toUpperCase();
+});
+//******************************************************
+
+app.use(function (req, res, next) {
+    var now = new Date().toString();
+    var log = now + ":" + req.method + ":" + req.url;
+
+    console.log(log);
+    fs.writeFile('server.log', log + "\n", function (err) {
+        if (err) {
+            console.log("cannt append to log file");
+        }
+    });
+    next();
+});
+
+app.use(function (req, res, next) {
+   res.render("maintenance.hbs");
+});
+
+// must be after app.use Methods.
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    // res.send("hello Express");
+    // res.send("<h1>hello Express</h1>");
+    res.send({
+        name: "ibrahim",
+        info: [
+            "25/08/1993",
+            "Mansoura"
+        ]
+    });
+});
+
+app.get('/home', function (req, res) {
+    res.render('home.hbs', {
+        pageTitle: 'home page',
+        welcomeMessage: "Welcome to our website"
+    })
+});
+
+
+app.get('/about', function (req, res) {
+    res.render('about.hbs', {
+        pageTitle: 'about page'
+    })
+});
+
+app.get('/bad', function (req, res) {
+    res.send({
+        errorMessage: "unable to handel request"
+    });
+});
+
+
+app.listen(3000, function () {
+    console.log('server is up on port : 3000');
+});
